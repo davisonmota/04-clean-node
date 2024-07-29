0bedd2db9d64438cd42080c15509396dbb38444a
Davison Mota <davimotagnr@gmail.com>
2024-07-29 12:04:21 -0300

    Aggregates & Watched Lists - Teste de edição da pergunta

--- src/domain/forum/application/use-cases/edit-question.spec.ts
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -1,6 +1,8 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { InMemoryQuestionAttachmentsRepository } from '@/infra/repositories/in-memory-question-attachments-repository'
 import { InMemoryQuestionsRepository } from '@/infra/repositories/in-memory-question-repository'
 import { makeQuestion } from 'test/factories/make-question'
+import { makeQuestionAttachment } from 'test/factories/make-question-attachment'
 import { describe, expect, test } from 'vitest'
 import { NotAllowedError } from '../errors/not-allowed-error '
 import { EditQuestionUseCase } from './edit-question'
@@ -8,7 +10,13 @@ import { EditQuestionUseCase } from './edit-question'
 describe('Edit Question Use Case', () => {
   test('deve editar uma dúvida (question)', async () => {
     const inMemoryRepositoryQuestions = new InMemoryQuestionsRepository()
-    const editQuestion = new EditQuestionUseCase(inMemoryRepositoryQuestions)
+    const inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+
+    const editQuestion = new EditQuestionUseCase(
+      inMemoryRepositoryQuestions,
+      inMemoryQuestionAttachmentsRepository,
+    )
 
     const newQuestion = makeQuestion(
       {
@@ -19,11 +27,26 @@ describe('Edit Question Use Case', () => {
 
     await inMemoryRepositoryQuestions.create(newQuestion)
 
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: new UniqueEntityID(newQuestion.getId()),
+        attachmentId: new UniqueEntityID('1'),
+      }),
+    )
+
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: new UniqueEntityID(newQuestion.getId()),
+        attachmentId: new UniqueEntityID('2'),
+      }),
+    )
+
     await editQuestion.execute({
       userId: 'user-id',
       questionId: 'id-question',
       title: 'New title edited',
       content: 'New content edited',
+      attachmentsIds: ['1', '3'],
     })
 
     const questionEdited =
@@ -33,11 +56,30 @@ describe('Edit Question Use Case', () => {
     expect(questionEdited?.getContent()).toBe('New content edited')
     expect(questionEdited?.getAuthorId()).toBe('user-id')
     expect(questionEdited?.getId()).toBe('id-question')
+
+    expect(
+      inMemoryRepositoryQuestions.items[0]
+        .getAttachments()
+        .getItems()[0]
+        .getAttachmentId(),
+    ).toBe('1')
+    expect(
+      inMemoryRepositoryQuestions.items[0]
+        .getAttachments()
+        .getItems()[1]
+        .getAttachmentId(),
+    ).toBe('3')
   })
 
   test('Não deve editar uma dúvida (question) se não for o autor', async () => {
     const inMemoryRepositoryQuestions = new InMemoryQuestionsRepository()
-    const deleteQuestion = new EditQuestionUseCase(inMemoryRepositoryQuestions)
+    const inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+
+    const editQuestion = new EditQuestionUseCase(
+      inMemoryRepositoryQuestions,
+      inMemoryQuestionAttachmentsRepository,
+    )
 
     const newQuestion = makeQuestion(
       {
@@ -48,11 +90,12 @@ describe('Edit Question Use Case', () => {
 
     await inMemoryRepositoryQuestions.create(newQuestion)
 
-    const result = await deleteQuestion.execute({
+    const result = await editQuestion.execute({
       userId: 'any-user-id',
       questionId: 'id-question',
       title: 'New title edited',
       content: 'New content edited',
+      attachmentsIds: [],
     })
 
     expect(result.isLeft()).toBe(true)
